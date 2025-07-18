fa4b3e69aa28f0bda5e574a7d923b6f655212834
RyotaMiyahara1023 <nakaichi1728@gmail.com>
2025-07-18 18:56:28 +0900

    デザイン微調整

--- js/TextCount.js
+++ js/TextCount.js
@@ -7,6 +7,8 @@ textarea.addEventListener("input", () => {
     clearTimeout(timeoutId);
 
     timeoutId = setTimeout(() => {
+        textarea.style.height = "auto";
+        textarea.style.height = textarea.scrollHeight + "px";
         const length = textarea.value.length;
         counter.textContent = `文字数: ${length}`;
     }, 1000);
